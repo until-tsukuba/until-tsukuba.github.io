38152d7e111d27b9297280a4dee76191f7fa4110
Nakaya <eniehack@outlook.jp>
2023-03-20 01:46:14 +0900

    feature: feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,18 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy")
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin, {
     baseHref: "https://until-tsukuba.github.io/"
   });
+  eleventyConfig.addPlugin(pluginRss, {
+    posthtmlRenderOptions: {
+      closingSingleTag: "slash"
+    }
+  });
+  eleventyConfig.addNunjucksFilter("getNewestCollectionItemDate", pluginRss.getNewestCollectionItemDate)
+  eleventyConfig.addNunjucksFilter("dateToRfc3339", pluginRss.dateToRfc3339)
 
   return {
     dir: {
