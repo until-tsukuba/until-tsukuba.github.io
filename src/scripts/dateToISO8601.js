033e6fe10fc2137f7abf13276b48a7ffdbb09269
Nakaya <eniehack@outlook.jp>
2023-03-20 00:05:03 +0900

    feature: generate icalendar

---
+++ src/scripts/dateToISO8601.js
@@ -0,0 +1,15 @@
+function format(str) {
+  return ('00' + str).slice(-2);
+}
+module.exports = function(date) {
+  var formatted_date = "";
+  formatted_date += date.getUTCFullYear();
+  formatted_date += format(date.getUTCMonth() + 1);
+  formatted_date += format(date.getUTCDate())
+  formatted_date += "T"
+  formatted_date += format(date.getUTCHours());
+  formatted_date += format(date.getUTCMinutes());
+  formatted_date += format(date.getUTCSeconds());
+  formatted_date += "Z"
+  return formatted_date
+}
